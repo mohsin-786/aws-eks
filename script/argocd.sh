ff2141480f5698fe84146ae8f02a90b7db005728
Mohsin <109455638+mohsin-786@users.noreply.github.com>
2024-02-13 12:58:49 +0530

    Update argocd.sh

--- script/argocd.sh
+++ script/argocd.sh
@@ -13,7 +13,12 @@ sleep 120s
 
 export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'`
 
+echo $ARGOCD_SERVER > alb_host.txt
+
 export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+
+echo $ARGO_PWD > alb_pass.txt
+
 
 argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
 
